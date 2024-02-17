1996e12017bed880b79a4e13261dc534ac675274
Tariq Patel <tariqpatel9@gmail.com>
2024-02-17 08:35:39 +0200

    start with some styling and logic

--- RunningHill/RunningHill/Features/Home/HomeView.swift
+++ RunningHill/RunningHill/Features/Home/HomeView.swift
@@ -12,20 +12,44 @@ struct HomeView: View {
     @StateObject var homeViewModel = HomeViewModel()
     
     var body: some View {
+        VStack {
+            Text("Add words to make your sentence")
+            Text(homeViewModel.selectedWord)
+        }
+        
+        Text("sentence here")
+        
         VStack{
             List(homeViewModel.selectedList, id: \.self) { word in
                 Button(word) {
                     homeViewModel.selectedWord = word
                 }
             }
-            Text(homeViewModel.selectedWord)
-            Button("Verb") {
-                homeViewModel.selectWordList("verb")
-            }
-            Button("Noun") {
-                homeViewModel.selectWordList("noun")
-            }
         }
+        VStack{
+            HStack{
+                Button(action: {
+                    print("Button tapped!")
+                }) {
+                    Text("ADD WORD")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                }
+                Spacer()
+                Button(action: {
+                    print("Button tapped!")
+                }) {
+                    Text("SAVE SENTENCE")
+                        .frame(maxWidth: .infinity)
+                        .padding()
+                        .background(Color.blue)
+                        .foregroundColor(.white)
+                }
+            }.padding()
+        }
+        Spacer()
     }
 }
 
