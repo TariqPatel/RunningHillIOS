89591d7b0f2ce516884efad31fb799747ef15c09
Tariq Patel <tariqpatel9@gmail.com>
2024-02-17 06:21:10 +0200

    Project creation and initial commit

---
+++ RunningHill/RunningHill/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  RunningHill
+//
+//  Created by Tariq Patel on 2024/02/17.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ RunningHill/RunningHill/RunningHillApp.swift
@@ -0,0 +1,17 @@
+//
+//  RunningHillApp.swift
+//  RunningHill
+//
+//  Created by Tariq Patel on 2024/02/17.
+//
+
+import SwiftUI
+
+@main
+struct RunningHillApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
